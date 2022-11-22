0da0acc247beb6b8b69aad7d41ea2901febb5de1
emikoo <emikoo@users.noreply.github.com>
2022-11-22 10:02:44 +0100

    added api urls

---
+++ app/src/main/java/com/teenteen/ecommerceconcept/presentation/ui/utils/ApiConstants.kt
@@ -0,0 +1,8 @@
+package com.teenteen.ecommerceconcept.presentation.ui.utils
+
+object ApiConstants {
+    const val BASE_URL = "https://run.mocky.io/v3/"
+    const val PRODUCT_LIST_URL = "654bd15e-b121-49ba-a588-960956b15175"
+    const val PRODUCT_DETAIL_URL = "6c14c560-15c6-4248-b9d2-b4508df7d4f5"
+    const val CART_URL = "53539a72-3c5f-4f30-bbb1-6ca10d42c149"
+}
